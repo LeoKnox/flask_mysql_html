140d64437fc8ca3eed074bcb70eca45f21aaa7d1
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-01 15:45:40 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -6,6 +6,8 @@ mycursor = mydb.cursor()
 @app.route("/")
 @app.route("/index")
 def index():
+    mycursor.execute("ALTER TABLE flaskhtmldb ADD COLUMN char_id INT")
+    #mycursor.execute("CREATE TABLE user (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))")
     mycursor.execute("SHOW TABLES")
     for x in mycursor:
         print(x)
