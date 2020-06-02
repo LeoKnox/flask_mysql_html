750c5d1ece999c4d0914148f673232cf1e12ae91
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-01 17:05:07 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -6,9 +6,11 @@ mycursor = mydb.cursor()
 @app.route("/")
 @app.route("/index")
 def index():
-    mycursor.execute("ALTER TABLE flaskhtmldb ADD COLUMN char_id INT")
+    #mycursor.execute("ALTER TABLE flaskhtmldb ADD COLUMN char_id INT")
     #mycursor.execute("CREATE TABLE user (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))")
-    mycursor.execute("SHOW TABLES")
-    for x in mycursor:
-        print(x)
+    sql = "INSERT INTO user (name, email) VALUES (%s, %s)"
+    val = ("Elric", "Stormbringer")
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print (mycursor.rowcount, "record inserted")
     return render_template("index.html", nav_index="active")
